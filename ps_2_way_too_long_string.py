c5532c945c4604d1f83a9a70af4548cf78668513
fahad-md-kamal <faahad.hossain@gmail.com>
2021-05-20 12:05:29 +0600

    way too long string submitted

--- ps_2_way_too_long_string.py
+++ ps_2_way_too_long_string.py
@@ -1,17 +1,4 @@
-
-word = input()
-for i in range(int(word[0])):
-    if len(word) > 10:
-        c_len = len(word[1:-1])
-        print(f"{word[0]}{c_len}{word[-1]}")
-    elif len(word)>1:
-        print(word)
-
-
-# num = int(input())
-# for i in range(num):
-#     st = input()
-#     if len(st) > 10:
-#         print(f"{st[0]}{len(st)-2}{st[-1]}")
-#     else :
-#         print(st)
+for i in range(int(input())):
+    word = input()
+    result = f"{word[0]}{len(word)-2}{word[-1]}" if len(word) > 10 else word
+    print(result)
