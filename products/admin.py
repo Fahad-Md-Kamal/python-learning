02044fbd808a78050696b45e32abc2b57db94df5
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-01 17:14:11 +0600

    Project commited

---
+++ products/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from products.models import Product, Purchase
+
+admin.site.register(Product)
+admin.site.register(Purchase)
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import  User
+
+class Product(models.Model):
+    name = models.CharField(max_length=220)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Change required
+    date  = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"Sold {self.product.name} - {self.quantity} items for {self.total_price}"
+    
