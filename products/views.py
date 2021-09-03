4d8e573bb26c12611ff90fca5c77f1cce81e9a95
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-03 08:18:12 +0600

    aggragete functions to pandas lists

--- products/views.py
+++ products/views.py
@@ -4,18 +4,31 @@ import pandas as pd
 
 
 def chart_select_view(request):
+    error_message = None
+    df = None
+
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id'] = product_df['id']
-    # merge two arrays and drop repeting columns with drop()
-    # rename fields using dictionary with rename() funciton of 
-    # first dimention known as axis
-    df = pd.merge(purchase_df, product_df, on='product_id')\
-        .drop(['id_y', 'date_y'], axis=1)\
-        .rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+
+    if purchase_df.shape[0] > 0:
+        df = pd.merge(purchase_df, product_df, on='product_id')\
+            .drop(['id_y', 'date_y'], axis=1)\
+            .rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+
+        if request.method == 'POST':
+            chart_type = request.POST['sales']
+            date_form = request.POST['date_from']
+            date_to = request.POST['date_to']
+
+            df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            print(df['date'])
+            df2 =df.groupby('date', as_index=False)['total_price'].agg('sum')
+            print(df2)
+    else:
+        error_message = 'No records in the database'
+
     context = {
-        'products': product_df.to_html(),
-        'purchase': purchase_df.to_html(),
-        'df': df.to_html(),
+        'error_message': error_message,
     }
     return render(request, 'products/main.html', context)
