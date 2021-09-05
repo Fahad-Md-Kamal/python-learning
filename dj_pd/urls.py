c0f0e0ccb2ff906864583b9f10c26c9b922b68be
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-05 18:19:24 +0600

    Initial Data analysis Done

--- dj_pd/urls.py
+++ dj_pd/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import  settings
 from django.conf.urls.static import  static
+from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home-view'),
     path('performance/', include('products.urls', namespace='products')),
     path('upload', include('csvs.urls', namespace='csvs')),
     path('customers', include('customers.urls', namespace='customers')),
