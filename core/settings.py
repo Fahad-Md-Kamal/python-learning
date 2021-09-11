f698cca80af85ccc0a820d69ed937751c06f05cc
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-11 18:32:53 +0600

    context data for all pages posted

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories',
             ],
         },
     },
--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category, Product
+
+def categories(request):
+    return {
+        'categories':Category.objects.all(),
+    }
+
+
+def all_products(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'store/home.html', context)
