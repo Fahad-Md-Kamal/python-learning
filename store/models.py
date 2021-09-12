1ac9a34780c4ac4f27bbb6e59ee4af50187aab61
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-12 14:42:59 +0600

    Product and Category Presentation done

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -9,8 +10,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
-    # def get_absolute_url(self):
-    #     return reverse('categories', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -35,6 +36,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+    
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
+
 def categories(request):
     return {
-        'categories':Category.objects.all(),
+        'categories': Category.objects.all(),
     }
 
 
@@ -14,3 +15,21 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'store/home.html', context)
+
+
+def product_detail(request, slug):
+    print(request)
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    context = {
+        'product': product,
+    }
+    return render(request, 'store/products/detail.html', context)
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    context = {
+        'category': category,
+        'products': products,
+    }
+    return render(request, 'store/products/category.html', context)
