1acdc57925f8d94067851cbcbf78d81640ba0638
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-14 15:58:29 +0600

    Adding items to the basket.

--- basket/basket.py
+++ basket/basket.py
@@ -4,7 +4,7 @@ class Basket():
     A base Basket class, providing some default behaviors that 
     can be inherited or overrided, as necessary.
     """
-    
+
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get('skey')
@@ -12,12 +12,19 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         product_id = product.id
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
-        
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
 
 # from django.contrib.sessions.models import Session
 # s = Session.objects.get(pk='<get_session_key_form_browser_session>')
