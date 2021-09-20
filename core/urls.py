455281177cb3c37798d506d669792279c0bba973
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-20 18:33:33 +0600

    Registration Form Wired up

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
     path('basket/', include('basket.urls', namespace='basket')),
+    path('account/', include('account.urls', namespace='account')),
 ]
 
 if settings.DEBUG:
